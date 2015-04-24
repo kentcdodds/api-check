efde34eae7736771c5cccdd8a974519e55f6fe96
Kent C. Dodds <kent+github@doddsfamily.us>
2015-04-23 23:05:26 -0600

    Improving build scripts

--- webpack.config.js
+++ webpack.config.js
@@ -4,79 +4,73 @@ var _ = require('lodash-node');
 var packageJson = require('./package.json');
 var webpack = require('webpack');
 
-module.exports = getConfig();
+var env = process.env.NODE_ENV || 'development';
+var dev = env === 'development';
+var test = env === 'test';
+var prod = env === 'production';
 
-module.exports.getConfig = getConfig;
+var plugins = {
+  production: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ],
+  commonPost: [
+    new webpack.BannerPlugin(getBanner(), {raw: true})
+  ]
+};
+plugins.test = plugins.production;
 
-function getConfig(env) {
-  env = env || 'dev';
-  var dev = env === 'dev';
-  var test = env === 'test';
-  var prod = env === 'prod';
+var config = {
+  context: here('src'),
+  entry: test ? './index.test.js' : './index.js',
+  output: {
+    library: 'apiCheck',
+    libraryTarget: 'umd',
+    filename: dev ? 'api-check.js' : 'api-check.min.js',
+    path: here('dist'),
+    pathinfo: dev
+  },
 
-  var plugins = {
-    prod: [
-      new webpack.optimize.DedupePlugin(),
-      new webpack.optimize.OccurenceOrderPlugin(),
-      new webpack.optimize.AggressiveMergingPlugin(),
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false
-        }
-      })
-    ],
-    commonPost: [
-      new webpack.BannerPlugin(getBanner(), {raw: true})
+
+  stats: {
+    colors: true,
+    reasons: true
+  },
+
+  devtool: prod ? 'source-map' : 'inline-source-map',
+
+  plugins: _.filter(_.union(plugins[env], plugins.commonPost)),
+
+  resolve: {
+    extensions: ['', '.js']
+  },
+
+  resolveLoader: {
+    modulesDirectories: ['webpack/loaders', 'node_modules'],
+    root: here()
+  },
+
+  externals: {},
+
+  module: {
+    loaders: [
+      {test: /\.js$/, loader: 'babel!jshint', exclude: /node_modules/}
     ]
-  };
-  plugins.test = plugins.prod;
+  },
+  jshint: {
+    failOnHint: true,
+    emitErrors: true
+  }
+};
 
-  var config = {
-    context: here('src'),
-    entry: test ? './index.test.js' : './index.js',
-    output: {
-      library: 'apiCheck',
-      libraryTarget: 'umd',
-      filename: dev ? 'api-check.js' : 'api-check.min.js',
-      path: here('dist'),
-      pathinfo: dev
-    },
-
-
-    stats: {
-      colors: true,
-      reasons: true
-    },
-
-    devtool: prod ? 'source-map' : 'inline-source-map',
-
-    plugins: _.filter(_.union(plugins[env], plugins.commonPost)),
-
-    resolve: {
-      extensions: ['', '.js']
-    },
-
-    resolveLoader: {
-      modulesDirectories: ['webpack/loaders', 'node_modules'],
-      root: here()
-    },
-
-    externals: {},
-
-    module: {
-      loaders: [
-        {test: /\.js$/, loader: 'babel!jshint', exclude: /node_modules/}
-      ]
-    },
-    jshint: {
-      failOnHint: true,
-      emitErrors: true
-    }
-  };
-
-  console.log('Config is in ' + env + ' mode');
-  return config;
-}
+console.log('Config is in ' + env + ' mode');
+module.exports = config;
 
 
 function getBanner() {
